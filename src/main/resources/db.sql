6f0c8a0cd862c6ef271dfea29851f0d6fc6bd829
GaryHuang <834865081@qq.com>
2017-08-18 15:38:35 +0800

    初始化项目

---
+++ src/main/resources/db.sql
@@ -0,0 +1,92 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.53-log : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `sys_privilege` */
+
+DROP TABLE IF EXISTS `sys_privilege`;
+
+CREATE TABLE `sys_privilege` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `url` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_privilege` */
+
+insert  into `sys_privilege`(`id`,`name`,`url`) values (1,'admin权限','/adminHello');
+insert  into `sys_privilege`(`id`,`name`,`url`) values (2,'sang权限','/sangHello');
+insert  into `sys_privilege`(`id`,`name`,`url`) values (3,'权限','/hello/list');
+
+/*Table structure for table `sys_role` */
+
+DROP TABLE IF EXISTS `sys_role`;
+
+CREATE TABLE `sys_role` (
+  `id` int(10) NOT NULL,
+  `name` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_role` */
+
+insert  into `sys_role`(`id`,`name`) values (1,'ROLE_ADMIN');
+insert  into `sys_role`(`id`,`name`) values (2,'ROLE_USER');
+
+/*Table structure for table `sys_role_privilege` */
+
+DROP TABLE IF EXISTS `sys_role_privilege`;
+
+CREATE TABLE `sys_role_privilege` (
+  `id` varchar(20) DEFAULT NULL,
+  `role_id` varchar(20) DEFAULT NULL,
+  `privilege_id` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_role_privilege` */
+
+insert  into `sys_role_privilege`(`id`,`role_id`,`privilege_id`) values ('1','1','1');
+insert  into `sys_role_privilege`(`id`,`role_id`,`privilege_id`) values ('2','2','2');
+
+/*Table structure for table `sys_user` */
+
+DROP TABLE IF EXISTS `sys_user`;
+
+CREATE TABLE `sys_user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_user` */
+
+insert  into `sys_user`(`id`,`username`,`password`) values (1,'root','root');
+insert  into `sys_user`(`id`,`username`,`password`) values (2,'sang','sang');
+
+/*Table structure for table `sys_user_roles` */
+
+DROP TABLE IF EXISTS `sys_user_roles`;
+
+CREATE TABLE `sys_user_roles` (
+  `user_id` int(10) DEFAULT NULL,
+  `role_id` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_user_roles` */
+
+insert  into `sys_user_roles`(`user_id`,`role_id`) values (1,1);
+insert  into `sys_user_roles`(`user_id`,`role_id`) values (2,2);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
