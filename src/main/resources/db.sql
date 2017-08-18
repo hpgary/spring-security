b809fe1f4b7d28e764c2c23386a34aeca865a66c
GaryHuang <834865081@qq.com>
2017-08-18 17:40:29 +0800

    更新关联表

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v12.09 (64 bit)
 MySQL - 5.5.53-log : Database - test
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -47,15 +48,17 @@ insert  into `sys_role`(`id`,`name`) values (2,'ROLE_USER');
 DROP TABLE IF EXISTS `sys_role_privilege`;
 
 CREATE TABLE `sys_role_privilege` (
-  `id` varchar(20) DEFAULT NULL,
+  `id` int(20) NOT NULL AUTO_INCREMENT,
   `role_id` varchar(20) DEFAULT NULL,
-  `privilege_id` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `privilege_id` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 /*Data for the table `sys_role_privilege` */
 
-insert  into `sys_role_privilege`(`id`,`role_id`,`privilege_id`) values ('1','1','1');
-insert  into `sys_role_privilege`(`id`,`role_id`,`privilege_id`) values ('2','2','2');
+insert  into `sys_role_privilege`(`id`,`role_id`,`privilege_id`) values (1,'1','1');
+insert  into `sys_role_privilege`(`id`,`role_id`,`privilege_id`) values (2,'2','2');
+insert  into `sys_role_privilege`(`id`,`role_id`,`privilege_id`) values (3,'1','3');
 
 /*Table structure for table `sys_user` */
 
